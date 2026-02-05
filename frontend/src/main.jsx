82757f029d48126a0cdc03416959bcc0564c06da
Zal-n <katonazalan816@gmail.com>
2026-02-05 22:46:20 +0100

    Added global context

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './style/index.css';
+import App from './pages/App';
+import { GlobalProvider } from './contexts/GlobalContext';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <GlobalProvider>
     <App />
-  </StrictMode>,
+  </GlobalProvider>
 )
