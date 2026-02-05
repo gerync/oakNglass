2ac5645b08282b1cdcb48d942d063df6840eb982
Zal-n <katonazalan816@gmail.com>
2026-02-05 22:46:48 +0100

    Login check, updated imports

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,43 +1,60 @@
 import { Navbar, Nav, NavDropdown, Image } from "react-bootstrap";
 import { BrowserRouter, Routes, Route, NavLink } from "react-router-dom";
+import '../style/Navbar.css';
 
-import Home from "../../Home";
-import Products from "../../Products";
+import Home from "../pages/Home";
+import Products from "../pages/Products";
+import AboutUs from '../pages/AboutUs';
+
+import { useContext } from "react";
+import { GlobalContext } from "../contexts/GlobalContext";
+
 
 function NavbarComponent() {
+  const { user } = useContext(GlobalContext);
+
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter expand="md" fixed="top" className="custom-navbar nav-inner">
         <Navbar expand="md" fixed="top" className="custom-navbar">
-          <Navbar.Brand className="brand"><Image src="src/assets/Logo.svg" height='50px'/></Navbar.Brand>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" className="custom-toggle ms-auto" />
+          <Navbar.Collapse className="nav-inner">
+            <Navbar.Brand as={NavLink} to='/' className="brand"><Image src="src/assets/Logo.svg" height='50px' /></Navbar.Brand>
             <Nav>
               <Nav.Link className="nav-link-custom" as={NavLink} to='/'>Kezdőlap</Nav.Link>
-              <Nav.Link className="nav-link-custom" as={NavLink} to='/shop'>Termékek</Nav.Link>
+              <Nav.Link className="nav-link-custom" as={NavLink} to='/termekek'>Termékek</Nav.Link>
+              <Nav.Link className="nav-link-custom" as={NavLink} to='/rolunk'>Rólunk</Nav.Link>
             </Nav>
             <Nav className="ms-auto">
-              <Nav.Link className="nav-link-custom">Bejelentkezés</Nav.Link>
+              {
+                !user ? (
+                  <Nav.Link className="nav-link-custom">Bejelentkezés</Nav.Link>
+                ) : null
+              }
 
-              <NavDropdown
-                title="Fiók"
-                id="basic-nav-dropdown"
-                className="nav-dropdown-custom "
-                drop="start"
-              >
-                <NavDropdown.Item>Rendelések</NavDropdown.Item>
-                <NavDropdown.Item >Kijelentkezés</NavDropdown.Item>
-              </NavDropdown>
+              {
+                user ? (
+                  <NavDropdown
+                    title="Fiók"
+                    id="basic-nav-dropdown"
+                    className="nav-dropdown-custom "
+                    drop="start"
+                  >
+                    <NavDropdown.Item>Rendelések</NavDropdown.Item>
+                    <NavDropdown.Item >Kijelentkezés</NavDropdown.Item>
+                  </NavDropdown>
+                ) : null
+              }
             </Nav>
           </Navbar.Collapse>
         </Navbar>
 
         <Routes>
-          <Route path="/" exact activeClassName="active" element={<Home/>}/>
-          <Route path="/shop" exact activeClassName="active" element={<Products/>}/>
+          <Route path="/" exact activeClassName="active" element={<Home />} />
+          <Route path="/termekek" exact activeClassName="active" element={<Products />} />
+          <Route path="/rolunk" exact activeClassName="active" element={<AboutUs />} />
         </Routes>
       </BrowserRouter>
-
     </>
   )
 
