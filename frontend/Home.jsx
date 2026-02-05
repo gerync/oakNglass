ee793d6c7a801b4c65cb572ea4fd7e1b63662323
Zal-n <katonazalan816@gmail.com>
2026-02-05 22:48:24 +0100

    directory restructuring

--- frontend/Home.jsx
+++
@@ -1,9 +0,0 @@
-function Home() {
-  return (
-    <>
-      <h1>Kezdőlap</h1>
-    </>
-  )
-}
-
-export default Home;
--- frontend/src/pages/App.jsx
+++ frontend/src/pages/App.jsx
@@ -1,6 +1,6 @@
-import './App.css';
+import '../style/App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from './components/Navbar'
+import Navbar from '../components/Navbar'
 
 
 function App() {
